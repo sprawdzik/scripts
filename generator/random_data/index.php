5d7aa6cb20883d2aec6e363a91c5ac4ebbba33c1
Prawdzik Sebastian <Sebastian.Prawdzik@asseco.pl>
2020-01-10 14:55:41 +0100

    Add pesel, company, regon and save  to file

--- generator/random_data/index.php
+++ generator/random_data/index.php
@@ -5,38 +5,45 @@ require_once "vendor/autoload.php";
 // config
 $cnfg_firstName = true;
 $cnfg_lastName = true;
-$cnfg_dob = true;
+$cnfg_pesel = true;
+$cnfg_dob = false;
 $cnfg_city = true;
-$cnfg_mail = true;
-$cnfg_password = true;
+$cnfg_mail = false;
+$cnfg_company = true;
+$cnfg_regon = true;
+$cnfg_password = false;
+
+$sep =  "\t"; // separator
+$filename = date('YmdHis').".txt";
 
 // use the factory to create a Faker/Generator instance
-$faker = Faker\Factory::create();
+$faker = Faker\Factory::create('pl_PL');
 
-for ($i = 0; $i < 100; $i++) {
+for ($i = 0; $i < 20000; $i++) {
 
-    echo person($i, $cnfg_firstName, $cnfg_lastName, $cnfg_dob, $cnfg_city,$cnfg_mail,$cnfg_password) . PHP_EOL;
+    echo $data = person($i) . PHP_EOL;
+    save($data);
 }
 
-function person($i, $cnfg_firstName, $cnfg_lastName, $cnfg_dob, $cnfg_city,$cnfg_mail,$cnfg_password)
+function person($i)
 {
-    global $faker;
+    global $faker, $sep, $cnfg_firstName, $cnfg_lastName, $cnfg_pesel, $cnfg_dob, $cnfg_city,$cnfg_mail,$cnfg_company, $cnfg_regon, $cnfg_password;
     $name = '';
     // start first name
     if ($cnfg_firstName) {
         if ($i == 0) {
-            $name = "Firstname" . "\t";
+            $name = "Firstname" . $sep;
         } else {
             $gender = random_int(0, 1);
             switch ($gender) {
                 case '0':
-                    $name = $faker->firstName('male') . "\t";
+                    $name = $faker->firstName('male') . $sep;
                     break;
                 case '1':
-                    $name = $faker->firstName('female') . "\t";
+                    $name = $faker->firstName('female') . $sep;
                     break;
                 default:
-                    $name = $faker->firstName() . "\t";
+                    $name = $faker->firstName() . $sep;
                     break;
             }
         }
@@ -46,52 +53,83 @@ function person($i, $cnfg_firstName, $cnfg_lastName, $cnfg_dob, $cnfg_city,$cnfg
     // start last name
     if ($cnfg_lastName) {
         if ($i == 0) {
-            $name .= "Lastname" . "\t";
+            $name .= "Lastname" . $sep;
         } else {
-            $name .= $faker->lastName() . "\t";
+            $name .= $faker->lastName() . $sep;
+        }
+    }
+    // end last name
+    // start pesel
+    if ($cnfg_pesel) {
+        if ($i == 0) {
+            $name .= "PESEL" . $sep;
+        } else {
+            $name .= $faker->pesel() . $sep;
         }
     }
     // end last name
     // start day of birth
     if ($cnfg_dob) {
         if ($i == 0) {
-            $name .= "DOB" . "\t";
+            $name .= "DOB" . $sep;
         } else {
-            $name .= $faker->date($format = 'Y-m-d', $max = 'now') . "\t";
+            $name .= $faker->date($format = 'Y-m-d', $max = 'now') . $sep;
         }
     }
     // end day of birth
     // start city
     if ($cnfg_city) {
         if ($i == 0) {
-            $name .= "City" . "\t";
+            $name .= "City" . $sep;
         } else {
-            $name .= $faker->city . "\t";
+            $name .= $faker->city . $sep;
         }
     }
     // end city
     // start mail
     if ($cnfg_mail) {
         if ($i == 0) {
-            $name .= "Mail" . "\t";
+            $name .= "Mail" . $sep;
         } else {
-            $name .= $faker->email . "\t";
+            $name .= $faker->email . $sep;
         }
     }
     // end mail
-    // start mail
+    // start company
+    if ($cnfg_company) {
+        if ($i == 0) {
+            $name .= "Company" . $sep;
+        } else {
+            $name .= $faker->company . $sep;
+        }
+    }
+    // end company
+    // start regon
+    if ($cnfg_regon) {
+        if ($i == 0) {
+            $name .= "Regon" . $sep;
+        } else {
+            $name .= $faker->regon . $sep;
+        }
+    }
+    // end regon
+    // start password
     if ($cnfg_password) {
         if ($i == 0) {
-            $name .= "Password" . "\t";
+            $name .= "Password" . $sep;
         } else {
-            $name .= $faker->password . "\t";
+            $name .= $faker->password . $sep;
         }
     }
-    // end mail
-
-    else {
-        $name = "No data";
-    }
+    // end password
 
     return $name;
+}
+
+function save($name){ // save data to file
+    global $filename;
+    $myfile = fopen($filename, "a") or die("Unable to open file!");
+    $txt = $name;
+    fwrite($myfile, $txt);
+    fclose($myfile);
 }
